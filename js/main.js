2e4798657274294663cf50fa7fb8aa7113cec155
NataBoginia <nata_boginia@mail.ru>
2019-06-19 17:54:53 +0300

    Add comments to the functions

--- js/main.js
+++ js/main.js
@@ -19,20 +19,36 @@ var COMMENTS = [
 
 var NAMES = ['Анна', 'Богдан', 'Виктория', 'Гена', 'Дима', 'Ева', 'Женя', 'Зина', 'Ира', 'Константин'];
 
-var createRandomInt = function (max, min, excludeMax) {
+/**
+  * Возвращает случайное целое число в заданном диапазоне,
+  * включая или исключая верхнюю границу диапазона
+  *
+  * @param {number} min - Минимальное значение диапазона
+  * @param {number} max - Максимальное значение дипазона
+  * @param {boolean} excludeMax - Признак того, что максимальное значение диапазона исключается при генерации случайного числа
+  * @return {number} Случайное целое число в заданном диапазоне
+  */
+var getRandomInt = function (min, max, excludeMax) {
   var randomNumber = Math.random() * (max - min) + min;
 
   return excludeMax ? Math.floor(randomNumber) : Math.round(randomNumber);
 };
 
+/**
+  * Генерирует тестовые комментарии в виде массива объектов заданного размера.
+  * Параметры каждого комментария задаются случайным образом, используя наборы тестовых данных
+  *
+  * @param {number} commentsCount - Количество комментариев (размер генерируемого массива)
+  * @return {Object[]} Массив комментариев
+  */
 var createComments = function (commentsCount) {
   var comments = [];
 
   for (var i = 0; i < commentsCount; i++) {
     var comment = {
-      avatar: 'img/avatar-' + createRandomInt(AVATAR_COUNT_MAX, AVATAR_COUNT_MIN, false) + '.svg',
-      message: createRandomInt(1, 0, false) ? COMMENTS[createRandomInt(COMMENTS.length, 0, true)] : (COMMENTS[createRandomInt(COMMENTS.length, 0, true)] + ' ' + COMMENTS[createRandomInt(COMMENTS.length, 0, true)]),
-      name: NAMES[createRandomInt(NAMES.length, 0, true)]
+      avatar: 'img/avatar-' + getRandomInt(AVATAR_COUNT_MIN, AVATAR_COUNT_MAX, false) + '.svg',
+      message: getRandomInt(0, 1, false) ? COMMENTS[getRandomInt(0, COMMENTS.length, true)] : (COMMENTS[getRandomInt(0, COMMENTS.length, true)] + ' ' + COMMENTS[getRandomInt(0, COMMENTS.length, true)]),
+      name: NAMES[getRandomInt(0, NAMES.length, true)]
     };
 
     comments[i] = comment;
@@ -41,14 +57,21 @@ var createComments = function (commentsCount) {
   return comments;
 };
 
+/**
+  * Генерирует тестовые фоторафии в виде массива объектов заданного размера.
+  * Параметры каждой фотографии задаются случайным образом, используя наборы тестовых данных
+  *
+  * @param {number} photosCount - Количество фотографий (размер генерируемого массива)
+  * @return {Object[]} Массив фотографий
+  */
 var createTestPhotos = function (photosCount) {
   var photos = [];
 
   for (var i = 1; i <= photosCount; i++) {
     var photo = {
       url: 'photos/' + i + '.jpg',
-      likes: createRandomInt(LIKES_COUNT_MAX, LIKES_COUNT_MIN, false),
-      comments: createComments(createRandomInt(COMMENTS_COUNT_MAX, COMMENTS_COUNT_MIN, false))
+      likes: getRandomInt(LIKES_COUNT_MIN, LIKES_COUNT_MAX, false),
+      comments: createComments(getRandomInt(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX, false))
     };
     photos[i - 1] = photo;
   }
@@ -56,6 +79,13 @@ var createTestPhotos = function (photosCount) {
   return photos;
 };
 
+/**
+  * Создает DOM-элемент для фотографии по шаблону
+  *
+  * @param {Object} photo - Объект фотографии
+  * @param {Object} template - DOM-элемент шаблона разметки
+  * @return {Object} DOM-элемент фотографии
+  */
 var renderPhoto = function (photo, template) {
   var photoElement = template.cloneNode(true);
 
@@ -66,6 +96,13 @@ var renderPhoto = function (photo, template) {
   return photoElement;
 };
 
+/**
+  * Возвращает DOM-фрагмент, наполненный DOM-элементами всех фотографий
+  *
+  * @param {Object[]} photos - Массив фотографий
+  * @param {Object} template - DOM-элемент шаблона разметки
+  * @return {Object} DOM-фрагмент
+  */
 var createPhotosBlock = function (photos, template) {
   var fragment = document.createDocumentFragment();
 
@@ -76,9 +113,9 @@ var createPhotosBlock = function (photos, template) {
   return fragment;
 };
 
+var photos = createTestPhotos(PHOTOS_COUNT);
+
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 var targetPhotoBlock = document.querySelector('.pictures');
-
-var photos = createTestPhotos(PHOTOS_COUNT);
 
 targetPhotoBlock.appendChild(createPhotosBlock(photos, pictureTemplate));
